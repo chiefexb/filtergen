f548a195bcf48b838aa3e68438597aa30b443969
chiefexb <chief_exb@mail.ru>
2014-07-10 19:16:45 +0400

    update 10.07.2014 Работает: умеет обрабатывать папку с xml файлами фильтров

--- filtergen.py
+++ filtergen.py
@@ -1,70 +1,81 @@
 #!/usr/bin/python
 #coding: utf8
 import sys
+from os import *
 from lxml import etree
 from cStringIO import StringIO
 import fdb
 def main():
  qt="'"
- if len(sys.argv)<=1:
-  print ("getfromint: нехватает параметров\nИспользование: getfromint ФАЙЛ_КОНФИГУРАЦИИ")
+ if len(sys.argv)<=5:
+  print ("getfromint: нехватает параметров\nИспользование: getfromint ХОСТ ИМЯ_БД ПОЛЬЗОВАТЕЛЬ ПАРОЛЬ ПАПКА [НОМЕР КАТЕГОРИИ]")
   sys.exit(2)
  print sys.argv[1]
 #Открытие файла конфигурации
  try:
-  f=file(sys.argv[1])
+  hostname=(sys.argv[1])
+ except Exception,e:
+  print e
+  sys.exit(2) 
+ try:
+  database=(sys.argv[2])
  except Exception,e:
   print e
   sys.exit(2)
  try:
-  parid=(sys.argv[2])
+  username=(sys.argv[3])
  except Exception,e:
   print e
   sys.exit(2)
- xml = etree.parse(f)
- xmlroot=xml.getroot()
- flt=xmlroot.find('CustomSqlFilter')
- SqlFilterName=flt.attrib['SqlFilterName']
-#.encode('CP1251')
- print "1"
- SqlJoinPart=flt.attrib['SqlJoinPart']
-#.encode('CP1251')
- #print 
- SqlWherePart=flt.attrib['SqlWherePart']
-#.encode('CP1251')
- print flt.attrib['SqlWherePart']
- SqlFilterDescription=flt.attrib['SqlFilterDescription']
-#.encode('CP1251')
- ObjectName=flt.attrib['ObjectName']
- genidsql='select gen_id (seq_custom_sql_filter,1) from rdb$database'
- sql='INSERT INTO CUSTOM_SQL_FILTER (ID, OBJECT_NAME, SQL_FILTER_NAME, SQL_JOIN_PART, SQL_WHERE_PART, SQL_FILTER_DESCRIPTION, IS_LIST_VISIBLE, IS_STAT_VISIBLE, IS_ANALYTICS_VISIBLE, PARENT_ID) VALUES (?,?,?,?,?,?,?,?,?,?)'
- #sql=sql+genid+", "+qt+"doc_ip_doc"+qt+", "
-#+qt + SqlFilterName + qt+", "+qt+SqlJoinPart+qt+ " , "+qt+SqlWherePart+ qt+", "+qt+SqlFilterDescription+qt+", 1, 1, 1,"+parid+" );"
-#17121000000120, 'doc_ip_doc', '111111', NULL, NULL, NULL, 1, 1, 1, 17121000000085);' 
- 
- username='SYSDBA'
- password='masterkey'
+ try:
+  password=(sys.argv[4])
+ except Exception,e:
+  print e
+  sys.exit(2)
  concodepage='WIN1251'
- hostname='localhost'
- database='ncore-fssp'
+ # hostname='localhost'
+ # database='ncore-fssp'
  try:
-  con = fdb.connect (host=hostname, database=database, user=username, password=password,charset=concodepage)
- except  Exception, e:
+  con = fdb.connect (host=hostname, database=database, user=username,  password=password,charset=concodepage) 
+ except   Exception, e:
   print("Ошибка при открытии базы данных:\n"+str(e))
   sys.exit(2)
  cur = con.cursor()
- print sql
- print SqlWherePart
- cur.execute(genidsql)
- r=cur.fetchall()
- genid=r[0][0]
- print genid
-#                   ID,    OBJECT_NAME, SQL_FILTER_NAME, SQL_JOIN_PART,SQL_WHERE_PART, SQL_FILTER_DESCRIPTION, IS_LIST_VISIBLE, IS_STAT_VISIBLE, IS_ANALYTICS_VISIBLE, PARENT_ID) VALUES (?)
- cur.execute (sql,(genid, ObjectName,  (SqlFilterName)   ,(SqlJoinPart), (SqlWherePart),   SqlFilterDescription  , 1 , 1 , 1,parid))
-#(sql.decode('UTF-8').encode(concodepage))
- con.commit()
+ try:
+  input_path=(sys.argv[5])
+ except Exception,e:
+  print e
+  sys.exit(2)
+#Цикл
+ for ff in listdir(input_path):
+  print "File:",ff
+  try:
+   parid=(sys.argv[6])
+  except Exception,e:
+   print e
+   sys.exit(2)
+  f=file(input_path+ff)
+  xml = etree.parse(f)
+  xmlroot=xml.getroot()
+  flt=xmlroot.find('CustomSqlFilter')
+  SqlFilterName=flt.attrib['SqlFilterName']
+  SqlJoinPart=flt.attrib['SqlJoinPart']
+  SqlWherePart=flt.attrib['SqlWherePart']
+  print flt.attrib['SqlWherePart']
+  SqlFilterDescription=flt.attrib['SqlFilterDescription']
+  ObjectName=flt.attrib['ObjectName']
+  genidsql='select gen_id (seq_custom_sql_filter,1) from rdb$database'
+  sql='INSERT INTO CUSTOM_SQL_FILTER (ID, OBJECT_NAME, SQL_FILTER_NAME, SQL_JOIN_PART, SQL_WHERE_PART, SQL_FILTER_DESCRIPTION, IS_LIST_VISIBLE, IS_STAT_VISIBLE, IS_ANALYTICS_VISIBLE, PARENT_ID) VALUES (?,?,?,?,?,?,?,?,?,?)'
+  print sql
+  print SqlWherePart
+  cur.execute(genidsql)
+  r=cur.fetchall()
+  genid=r[0][0]
+  print genid
+  cur.execute (sql,(genid, ObjectName,  (SqlFilterName)   ,(SqlJoinPart), (SqlWherePart),   SqlFilterDescription  , 1 , 1 , 1,parid))
+  con.commit()
+  f.close
  con.close()
- f.close()
 if __name__ == "__main__":
     main()
 
